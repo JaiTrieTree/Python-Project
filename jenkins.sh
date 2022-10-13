314ff6d96e7bd002d831098ffb0497389a76f4dc
Vamsi <vamsibalagam636@gmail.com>
2022-10-14 01:00:36 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,4 +4,5 @@ sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum update -y
 sudo yum install fontconfig java-11-openjdk -y
 sudo yum install jenkins -y
+sudo yum install cifs-utils -y
 sudo systemctl restart jenkins
