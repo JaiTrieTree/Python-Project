c21d68482ea320e16f5dc02dc9e1aa1a46ec90d2
Vamsi <vamsibalagam636@gmail.com>
2022-10-13 10:58:53 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,7 @@
-sudo su -
-yum update -y
-mount /var/lib/jenkins
-df -h (Used to find all partitions with mount location in short form)
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum install fontconfig java-11-openjdk
-yum install jenkins
-systemctl restart jenkins
+### INSTALLING JENKINS ######
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum update -y
+sudo yum install fontconfig java-11-openjdk -y
+sudo yum install jenkins -y
+sudo systemctl restart jenkins
